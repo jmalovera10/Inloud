9b81e8c19e4fb27831b2443bd4aa3fde1392868d
tf-venegas10 <tf.venegas10@uniandes.edu.co>
2017-07-09 19:14:50 -0500

    Logic client v1

--- Backend/Inloud_Backend/inloud-backend-logic/src/main/java/com/papitas/inloud/backend/ejbs/ClientLogic.java
+++ Backend/Inloud_Backend/inloud-backend-logic/src/main/java/com/papitas/inloud/backend/ejbs/ClientLogic.java
@@ -7,29 +7,47 @@ package com.papitas.inloud.backend.ejbs;
 
 import com.papitas.inloud.backend.entities.ClientEntity;
 import com.papitas.inloud.backend.entities.InvoiceEntity;
+import com.papitas.inloud.backend.exceptions.BusinessLogicException;
+import com.papitas.inloud.backend.persistence.ClientPersistence;
 import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 /**
  *
  * @author venegas
  */
+@Stateless
 public class ClientLogic {
+    @Inject
+    private ClientPersistence persistence ;
 
     public ClientEntity getClient(long id) {
-        //TODO
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return persistence.find(id);
     }
 
-    public ClientEntity putClient(long id, ClientEntity toEntity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ClientEntity putClient(long id, ClientEntity toEntity) throws BusinessLogicException {
+        if (toEntity==null) throw new BusinessLogicException("La entidad no puede ser nulla");
+        ClientEntity entity = persistence.find(id);
+        if (entity==null) throw new BusinessLogicException("Cliente no encontrado");
+        toEntity.setId(entity.getId());
+        if (!entity.getLogin().equals(toEntity.getLogin())) throw new BusinessLogicException("El login no es el correcto");
+        if (toEntity.getPassword().length()<8) throw new BusinessLogicException("Contraseña muy corta ");
+        //birthDate
+        //email
+        return persistence.update(toEntity);
     }
 
-    public ClientEntity postClient(ClientEntity toEntity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ClientEntity postClient(ClientEntity toEntity) throws BusinessLogicException {
+        if (toEntity.getPassword().length()<8) throw new BusinessLogicException("Contraseña muy corta ");
+        return persistence.create(toEntity);
     }
 
-    public List<InvoiceEntity> getInvoices(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<InvoiceEntity> getInvoices(long id) throws BusinessLogicException {
+        ClientEntity entity = persistence.find(id);
+        if (entity==null) throw new BusinessLogicException("Cliente no encontrado");
+        return entity.getInvoices();
+ 
     }
 
     
